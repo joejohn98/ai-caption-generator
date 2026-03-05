9b51a04b2bf83a190faec45a1e9d9d565f796f24
joejohn98 <joejohn048@gmail.com>
2026-03-05 17:14:12 +0530

    feat(auth): establish user authentication endpoint
    This commit introduces the necessary logic and routing for users to authenticate. It includes:
    - A controller function (`loginUser`) to handle incoming login requests, validate credentials using Zod schemas, and compare passwords against the stored hash.
    - An update to the authentication routes to expose the `/login` POST endpoint, directing requests to the new controller function.
    - Integration of `bcryptjs` for secure password comparison.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
 
 import User from "../models/user.model";
-import { registerSchema } from "../utils/validators/auth.validators";
+import {
+  loginSchema,
+  registerSchema,
+} from "../utils/validators/auth.validators";
 
 const registerUser = async (req: Request, res: Response): Promise<void> => {
   const { username, email, password } = req.body;
@@ -42,4 +46,53 @@ const registerUser = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-export { registerUser };
+const loginUser = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+
+  const validate = loginSchema.safeParse({ email, password });
+
+  if (!validate.success) {
+    res.status(400).json({
+      status: "failed",
+      error: validate.error?.issues[0]?.message || "Invalid input",
+    });
+    return;
+  }
+
+  const existingUser = await User.findOne({ email });
+
+  if (!existingUser) {
+    res.status(400).json({
+      status: "failed",
+      error: "User with this email does not exist",
+    });
+    return;
+  }
+
+  try {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password,
+    );
+    if (!isPasswordValid) {
+      res.status(401).json({
+        status: "failed",
+        error: "Invalid email or password",
+      });
+      return;
+    }
+    res.status(200).json({
+      status: "success",
+      message: "User logged in successfully",
+      data: existingUser,
+    });
+  } catch (error) {
+    console.log("error to login the user", error);
+    res.status(500).json({
+      status: "failed",
+      error: "Internal server error, failed to login user",
+    });
+  }
+};
+
+export { registerUser, loginUser };
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controller";
+import { loginUser, registerUser } from "../controllers/auth.controller";
 
 const router = Router();
 
-router.post("/register", registerUser)
-// router.post("/login")
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
 
 export default router;
